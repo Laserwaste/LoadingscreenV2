4a08de853ba1b7fe5d1d988a55d5354d9b35248e
Laserwaste <permataalkautsar1@gmail.com>
2026-09-25 13:41:12 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿int totalstep = 5;
+int step = 1;
+while (step <= totalstep)
+
+{
+    Console.WriteLine($"Loading.. Step {step} of {totalstep} ");
+    step++;
+}
+Console.WriteLine("\nDungeon is ready, Enter Now!");
